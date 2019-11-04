a6847fbb0bd553ebf53007885b4bce6fdfb540c9
Petter Bøe Hørtvedt <petterho@nmbu.no>
2019-11-04 01:09:28 +0100

    Started on the project

---
+++ src/personal_projects/Dijkstra/djikstra_class.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Petter Bøe Hørtvedt'
+__email__ = 'petterho@nmbu.no'
+
+from src.personal_projects.Dijkstra.test_nodes import path_dict
+
+class Dijkstra:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.position = None
+
+    def shortest_path(self, start, stop):
+        pass
+
+
+
